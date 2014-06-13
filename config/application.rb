1ad2ca743614a801392814abf1405c123b0b2a98
Fabio Rehm <fgrehm@gmail.com>
2014-06-13 15:08:54 -0300

    Set action mailer delivery method to letter_opener_web on all environments

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module LetterOpenerWebDemo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.action_mailer.delivery_method = :letter_opener_web
   end
 end
